e80b1c429929380f69d41ddf436f1ce61e960d09
jairalcon <98365593+jairalcon@users.noreply.github.com>
2022-12-08 15:53:36 -0600

    updating format

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import Create from './components/create';
-import Read from './components/read';
-import Update from './components/update';
-import { Route, Routes } from 'react-router-dom';
-import NavBar from './components/navbar';
+import './App.css'
+import Create from './components/create'
+import Read from './components/read'
+import Update from './components/update'
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/navbar'
 
 export default function App() {
   return (
     <div className="main container col-8">
-    <NavBar />
-    <h2 className='main-header'>React Crud Operations App</h2>
+      <NavBar />
+      <h2 className="main-header">React Crud Operations App</h2>
       <Routes>
-        <Route path='/' element={<Create />} />
-        <Route path='/read' element={<Read />} />
-        <Route path='/update' element={<Update />} />
+        <Route path="/" element={<Create />} />
+        <Route path="/read" element={<Read />} />
+        <Route path="/update" element={<Update />} />
       </Routes>
     </div>
-  );
+  )
 }
